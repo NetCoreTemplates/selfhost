392cf4042e9b65553dce69e4d1681df5b368cfa2
Demis Bellot <demis.bellot@gmail.com>
2017-11-30 12:49:20 -0500

    Add minimal selfhost .NET Core 2.0 template

---
+++ MyApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Funq;
+using ServiceStack;
+using MyApp.ServiceInterface;
+using MyApp.ServiceModel;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5000/")
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseServiceStack(new AppHost());
+
+            app.Run(context =>
+            {
+                context.Response.Redirect("/metadata");
+                return Task.FromResult(0);
+            });
+        }
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("MyApp", typeof(MyServices).Assembly) { }
+
+        public override void Configure(Container container)
+        {
+        }
+    }
+}
