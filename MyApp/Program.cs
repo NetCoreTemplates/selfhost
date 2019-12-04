689d10e108b368b7ac2d08b3687013138267fd14
Demis Bellot <demis.bellot@gmail.com>
2019-12-03 23:33:52 -0500

    Upgrade to .NET Core 3.1 LTS

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Funq;
 using Microsoft.Extensions.Configuration;
@@ -20,7 +21,7 @@ namespace MyApp
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>()
+                .UseModularStartup<Startup>()
                 .UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://localhost:5000/")
                 .Build();
 
@@ -30,8 +31,6 @@ namespace MyApp
 
     public class Startup : ModularStartup
     {
-        public Startup(IConfiguration configuration) : base(configuration){}
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public new void ConfigureServices(IServiceCollection services)
@@ -39,7 +38,7 @@ namespace MyApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseServiceStack(new AppHost());
 
